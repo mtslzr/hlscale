938b85b9b5e3dbcd69171bbd55c72e31eba59493
Matthew Salazar <m@tthewsalazar.com>
2020-12-27 20:25:14 -0500

    Minor refactoring; removed stale tfstate

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/mtslzr/hlscale/pkg/cwevents"
-	"github.com/mtslzr/hlscale/pkg/scale"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/mtslzr/hlscale/pkg/constants"
+	"github.com/mtslzr/hlscale/pkg/cwevents"
 	"github.com/mtslzr/hlscale/pkg/exams"
+	"github.com/mtslzr/hlscale/pkg/scale"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -39,13 +39,16 @@ type Handler struct {
 // Handle is a pseudo-router to set up the function to run.
 func (h Handler) Handle(ctx context.Context, event Event) (events.APIGatewayProxyResponse, error) {
 	if len(event.Records) > 0 {
+
 		log.Infof("Running %s...", constants.CreateEvent)
 		record, err := cwevents.ParseRecord(event.Records[0].Change.NewImage)
 		if err != nil {
 			return sendResponse(err)
 		}
 		return sendResponse(cwevents.CreateEvents(record))
+
 	} else {
+
 		var body Body
 		err := json.Unmarshal([]byte(event.Body), &body)
 		if err != nil {
@@ -71,23 +74,28 @@ func (h Handler) Handle(ctx context.Context, event Event) (events.APIGatewayProx
 
 		}
 		return sendResponse(errors.New("unknown or missing function"))
+
 	}
 }
 
 func sendResponse(err error) (events.APIGatewayProxyResponse, error) {
 	var resp events.APIGatewayProxyResponse
 	if err == nil {
+
 		resp = events.APIGatewayProxyResponse{
 			StatusCode:      200,
 			Body:            "Success!",
 			IsBase64Encoded: false,
 		}
+
 	} else {
+
 		resp = events.APIGatewayProxyResponse{
 			StatusCode:      500,
 			Body:            "Error!",
 			IsBase64Encoded: false,
 		}
+
 	}
 	return resp, err
 }
